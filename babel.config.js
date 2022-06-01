3f9dff9e88343601b0e373bc9e8f4742ed194ff9
Quyet. Nguyen Van - CMC Global DE1 <nvquyet@cmcglobal.vn>
2022-06-01 16:44:42 +0700

    add prettier, eslint, husky

--- babel.config.js
+++ babel.config.js
@@ -2,27 +2,27 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     "@babel/preset-react",
-    "@babel/preset-typescript",
+    "@babel/preset-typescript"
   ],
   plugins: [
     [
       "@babel/plugin-transform-runtime",
       {
-        regenerator: true,
-      },
+        regenerator: true
+      }
     ],
     "@babel/plugin-proposal-numeric-separator",
     "@babel/plugin-proposal-optional-chaining",
     [
       "@babel/plugin-proposal-decorators",
       {
-        decoratorsBeforeExport: true,
-      },
+        decoratorsBeforeExport: true
+      }
     ],
     "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-nullish-coalescing-operator",
     "react-intl-auto",
     "@babel/plugin-proposal-class-properties",
-    ["transform-require-context"],
-  ],
+    ["transform-require-context"]
+  ]
 };
